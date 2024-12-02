c0524d49077ec315cc179a4253bf61cbf8bf3ab2
giova <giduarte850@gmail.com>
2024-12-02 18:01:15 -0300

    atualizandohome

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-document.getElementById('action-btn').addEventListener('click', () => {
-    alert('Welcome to the future!');
+// Evento de clique no botão de pesquisa
+document.querySelector('.search-bar button').addEventListener('click', () => {
+    const query = document.querySelector('.search-bar input').value;
+    console.log(`Pesquisa: ${query}`);
+    alert(`Você pesquisou por: "${query}"`);
   });
   
